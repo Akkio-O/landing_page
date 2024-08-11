4c9ee2e3a8940eba55450968483a2fe128c8fd9b
Eugene <zheka12620@gmail.com>
2024-08-12 01:18:01 +0300

    FrontEnd-desktop-finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import { effectHamburger } from './header/hamburger.mjs';
 import dataTariff from './modal/application.mjs';
 import init from './map/map.mjs';
+import media from './media/media.mjs';
 
 document.addEventListener('DOMContentLoaded', () => {
     dataTariff();
---
+++ src/js/media/media.mjs
@@ -0,0 +1,65 @@
+// sizes
+const mediaQuery = window.matchMedia('(max-width: 979px)');
+// div
+const contactsWrapper = document.querySelector('.contacts__wrapper');
+// img
+const planetCommunity = document.querySelector('.crmRequest__wrapper_img');
+const blockPhone = document.querySelector('.usefully__wrapper_device');
+//wrapper
+const usefullyWrapper = document.querySelector('.usefully__wrapper');
+const crmRequestWrapper = document.querySelector('.crmRequest__wrapper');
+const feedBackQuestionTittle = document.querySelector('.feedBackQuestion__wrapper_tittle');
+//text
+const textBlocks = document.querySelectorAll('.usefully__wrapper_text p');
+const blockPhoneNewText = [
+    'Любому бизнесу, работающему с клиентской базой',
+    'Бизнесу, управляющему ассортиментом',
+    'Бизнесу, где есть задачи, планирование, отчетность и KPI'
+];
+const feedBackQuestionNewText = document.createElement('h4');
+feedBackQuestionNewText.innerHTML = "Команда Alpha marketing";
+
+const contactsWrapperLink = document.createElement('div');
+contactsWrapperLink.classList.add('contacts__wrapper_link');
+contactsWrapperLink.innerHTML = `
+                <a class="contacts__wrapper_link_active" href="./index" class="active">
+                    <h2>МЕНЮ</h2>
+                </a>
+                <a href="#" class="active">
+                    <h2>УСЛУГИ</h2>
+                </a>
+                <a href="#" class="active">
+                    <h2>КЕЙСЫ</h2>
+                </a>
+                <a href="#" class="active">
+                    <h2>ЕЩЁ</h2>
+                </a>`;
+
+// function
+const originalTexts = Array.from(textBlocks).map(block => block.textContent);
+function handleTabletChange(e) {
+    if (e.matches) {
+        // window <= 980px
+        textBlocks.forEach((block, index) => {
+            block.textContent = originalTexts[index] || block.textContent;
+        });
+        planetCommunity.remove();
+        blockPhone.remove();
+        feedBackQuestionNewText.remove();
+        contactsWrapperLink.remove();
+    } else {
+        textBlocks.forEach((block, index) => {
+            block.textContent = blockPhoneNewText[index] || block.textContent;
+        });
+        usefullyWrapper.appendChild(blockPhone);
+        crmRequestWrapper.appendChild(planetCommunity);
+        feedBackQuestionTittle.appendChild(feedBackQuestionNewText)
+        contactsWrapper.insertBefore(contactsWrapperLink, contactsWrapper.firstChild);
+    }
+}
+
+mediaQuery.addListener(handleTabletChange);
+
+handleTabletChange(mediaQuery);
+
+export default handleTabletChange;
